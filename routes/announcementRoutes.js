c250315588b275b9e1557a171ede5e6983dbfedb
BadTemper7 <leal04.andrew@gmail.com>
2025-11-17 10:26:30 +0800

    updated server

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -11,12 +11,12 @@ import {
 const router = express.Router();
 
 // Admin routes
-router.post("/", createAnnouncement);
+// router.post("/", createAnnouncement);
 router.get("/", getAnnouncements);
-router.get("/:id", getAnnouncementById);
-router.put("/:id", updateAnnouncement);
-router.patch("/:id/status", updateAnnouncementStatus);
-router.delete("/:id", deleteAnnouncement);
+// router.get("/:id", getAnnouncementById);
+// router.put("/:id", updateAnnouncement);
+// router.patch("/:id/status", updateAnnouncementStatus);
+// router.delete("/:id", deleteAnnouncement);
 
 // router.get("/", getSampleResult);
 
