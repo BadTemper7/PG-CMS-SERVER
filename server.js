95e7773a7acba578edcf55fcac54e79714f594db
BadTemper7 <leal04.andrew@gmail.com>
2025-11-17 10:04:32 +0800

    updated server

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import announcementRoutes from "./routes/announcementRoutes.js";
+import { connectDB } from "./utils/db.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+await connectDB();
 
 app.use("/api/announcements", announcementRoutes);
 app.get("/", (req, res) => {
